972ace59e1f519bc32504c9eeff611777723905a
Daniela Dominguez Peña <daniela.dominguezz.p@gmail.com>
2021-09-29 23:28:54 -0500

    nuevos ejercicios

---
+++ 20-try-catch.js
@@ -0,0 +1,16 @@
+/* Try nos permite ejecutar código propenso a fallar de una manera segura */
+
+const gravedad = 9.8;
+console.log(gravedad);
+
+/* intenta ejecutar el código, si hay un error ejecuta el catch*/
+try {
+    gravedad = 10.1;
+} catch(e) {
+    /* El catch se ejecuta si hay un error, recibe como primer parametro el error */
+    console.log(e);
+} finally {
+    /* El finally siempre se ejecuta */
+    console.log('Soy el finally')
+}
+console.log(gravedad);
